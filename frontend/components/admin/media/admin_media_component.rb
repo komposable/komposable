7a01642c9de6142ae5e9d14ec1a2dc66c8bfa93b
Hans Lemuet <hans@etaminstudio.com>
2018-04-05 15:18:56 +0200

    Improved reliability of admin/media

--- frontend/components/admin/media/admin_media_component.rb
+++ frontend/components/admin/media/admin_media_component.rb
@@ -1,3 +1,13 @@
 module AdminMediaComponent
   extend ComponentHelper
+
+  property :media, required: true
+
+  def image
+    begin
+      @media.variant(resize: "150x150").processed.service_url
+    rescue ActiveStorage::InvariableError
+      @media.service_url
+    end
+  end
 end
