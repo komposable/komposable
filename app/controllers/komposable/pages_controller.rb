f43d2d5066fba2bd5a36493161badb6e621bb51c
Hans Lemuet <hans@etaminstudio.com>
2018-04-09 19:18:32 +0200

    Added missing NPM deps + cleanup comments in PageController

--- app/controllers/komposable/pages_controller.rb
+++ app/controllers/komposable/pages_controller.rb
@@ -1,15 +1,4 @@
 module Komposable
   class PagesController < BaseController
-    # skip_before_action :set_class_variables
-
-    # def index
-    #   @items = policy_scope(Komposable::Page)
-    # end
-
-    # private
-    #
-    # def set_class_variables(class_name = nil)
-    #   @klass_name = "Pages"
-    # end
   end
 end
