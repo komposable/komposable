cfdf984e5f7bcb810533823e061d3512eb5fa26b
Hans Lemuet <hans@spone.net>
2019-10-24 10:36:53 +0200

    Cleanup tests

--- test/controllers/presentations_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class PresentationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get presentation_url(presentations(:one))
-    assert_response :success
-  end
-end
--- test/controllers/trends_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class TrendsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get trend_url(id: "fake-fakations")
-    assert_response :success
-  end
-end
