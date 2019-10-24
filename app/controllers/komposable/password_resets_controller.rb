181c7154707b770efab3c5a5dc4c8534f01c44ae
Hans Lemuet <hans@spone.net>
2019-10-24 10:31:53 +0200

    Code style improvements

--- app/controllers/komposable/password_resets_controller.rb
+++ app/controllers/komposable/password_resets_controller.rb
@@ -4,6 +4,7 @@ module Komposable
     # before_action :require_login
     # is not declared in your ApplicationController.
     skip_before_action :require_login
+    before_action :set_user, only: %i[edit update]
     skip_after_action :verify_authorized
 
     def new
@@ -19,42 +20,36 @@ module Komposable
       # their password (a url with a random token)
       @user&.deliver_reset_password_instructions!
 
-      # Tell the user instructions have been sent whether or not email was found.
-      # This is to not leak information to attackers about which emails exist
-      # in the system.
-      redirect_to(login_path, notice: 'Instructions have been sent to your email.')
+      # Tell the user instructions have been sent whether or not email was
+      # found. This is to not leak information to attackers about which emails
+      # exist in the system.
+      redirect_to(login_path, notice: t("messages.instructions_sent"))
     end
 
     # This is the reset password form.
     def edit
-      @token = params[:id]
-      @user = User.load_from_reset_password_token(params[:id])
-
-      if @user.blank?
-        not_authenticated
-        return
-      end
     end
 
     # This action fires when the user has sent the reset password form.
     def update
-      @token = params[:id]
-      @user = User.load_from_reset_password_token(params[:id])
-
-      if @user.blank?
-        not_authenticated
-        return
-      end
-
       # the next line disables the password confirmation
       @user.password_confirmation = params[:user][:password]
 
       # the next line clears the temporary token and updates the password
       if @user.change_password(params[:user][:password])
-        redirect_to(login_path, notice: 'Your password was successfully changed, please log in.')
+        redirect_to(login_path, notice: t("messages.password_changed"))
       else
         render action: "edit"
       end
+    end
+
+    private
+
+    def set_user
+      @token = params[:id]
+      @user = User.load_from_reset_password_token(params[:id])
+
+      return not_authenticated if @user.blank?
     end
   end
 end
