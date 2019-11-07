6258519bc9dcf912aa26e2b7f8ec33933037a4e5
Hans Lemuet <Spone@users.noreply.github.com>
2019-11-07 13:57:08 +0100

    Limit user_sessions routes to new, create, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Komposable::Engine.routes.draw do
   root "users#index"
   resources :medias
-  resources :user_sessions
+  resources :user_sessions, only: [:new, :create, :destroy]
   resources :users
   get 'login' => 'user_sessions#new', as: :login
   post 'logout' => 'user_sessions#destroy', as: :logout
