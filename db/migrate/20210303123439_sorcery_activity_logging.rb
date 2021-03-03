3330c1afa85e3f7a55deebb11c779d6f225d43d8
Hans Lemuet <Spone@users.noreply.github.com>
2021-03-03 13:42:25 +0100

    Fix table name in migration

--- db/migrate/20210303123439_sorcery_activity_logging.rb
+++ db/migrate/20210303123439_sorcery_activity_logging.rb
@@ -1,10 +1,10 @@
 class SorceryActivityLogging < ActiveRecord::Migration[5.2]
   def change
-    add_column :User, :last_login_at,     :datetime, default: nil
-    add_column :User, :last_logout_at,    :datetime, default: nil
-    add_column :User, :last_activity_at,  :datetime, default: nil
-    add_column :User, :last_login_from_ip_address, :string, default: nil
+    add_column :users, :last_login_at,     :datetime, default: nil
+    add_column :users, :last_logout_at,    :datetime, default: nil
+    add_column :users, :last_activity_at,  :datetime, default: nil
+    add_column :users, :last_login_from_ip_address, :string, default: nil
 
-    add_index :User, [:last_logout_at, :last_activity_at]
+    add_index :users, [:last_logout_at, :last_activity_at]
   end
 end
